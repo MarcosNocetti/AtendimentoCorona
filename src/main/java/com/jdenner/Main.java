31ed1d8a843a04a160ae8cbd5240369be80bc8fb
Marcos Nocetti <nocettimarcos@gmail.com>
2020-04-21 19:04:34 -0300

    Driver SQL

--- src/main/java/com/jdenner/Main.java
+++ src/main/java/com/jdenner/Main.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jdenner;
 
+import com.jdenner.gui.LoginT;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
+ * Classe principal que inicia a aplicação
  *
- * @author gabri
+ * @author Juliano
  */
 public class Main {
-    
+
+    /**
+     * Método responsável por iniciar a aplicação
+     *
+     * @param args lista de parâmetros do método main
+     */
+    public static void main(String[] args) {
+        lookAndFeel();
+       
+            LoginT login;
+        try {
+            login = new LoginT();
+            login.setVisible(true);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+    }
+
+    private static void lookAndFeel() {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JDialog.setDefaultLookAndFeelDecorated(true);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            JOptionPane.showMessageDialog(null, e, "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
 }
