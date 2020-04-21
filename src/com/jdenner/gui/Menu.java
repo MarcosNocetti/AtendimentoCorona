821cb37b42655d0899db1c94e8e88622a1522d2d
Marcos Nocetti <nocettimarcos@gmail.com>
2020-04-21 19:18:22 -0300

    Menut hotfix

--- src/com/jdenner/gui/Menu.java
+++ src/com/jdenner/gui/Menu.java
@@ -17,8 +17,6 @@ public class Menu extends javax.swing.JFrame {
     private void initComponents() {
 
         desktopPane = new javax.swing.JDesktopPane();
-        jLabel1 = new javax.swing.JLabel();
-        lblUser = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sistema de Vendas");
@@ -27,15 +25,6 @@ public class Menu extends javax.swing.JFrame {
         desktopPane.setForeground(new java.awt.Color(153, 204, 255));
         desktopPane.setName("btDespesas"); // NOI18N
         desktopPane.setOpaque(false);
-
-        jLabel1.setText("Usuario em uso:");
-        desktopPane.add(jLabel1);
-        jLabel1.setBounds(730, 590, 80, 40);
-
-        lblUser.setText("User");
-        desktopPane.add(lblUser);
-        lblUser.setBounds(820, 594, 60, 30);
-
         getContentPane().add(desktopPane, java.awt.BorderLayout.CENTER);
         desktopPane.getAccessibleContext().setAccessibleName("Despesas");
 
@@ -45,7 +34,5 @@ public class Menu extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane desktopPane;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel lblUser;
     // End of variables declaration//GEN-END:variables
 }
