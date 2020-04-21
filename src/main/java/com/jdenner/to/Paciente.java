a5452244140eece15b2ae226cbb77917cdb13b98
Marcos Nocetti <nocettimarcos@gmail.com>
2020-04-21 15:19:12 -0300

    Exemplo TableModel classe Paciente

--- src/main/java/com/jdenner/to/Paciente.java
+++ src/main/java/com/jdenner/to/Paciente.java
@@ -10,5 +10,42 @@ package com.jdenner.to;
  * @author gabri
  */
 public class Paciente {
-    
+
+    public int getPacienteID() {
+        return PacienteID;
+    }
+
+    public void setPacienteID(int PacienteID) {
+        this.PacienteID = PacienteID;
+    }
+
+    public String getDescSintomas() {
+        return DescSintomas;
+    }
+
+    public void setDescSintomas(String DescSintomas) {
+        this.DescSintomas = DescSintomas;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+ 
+    int PacienteID;
+    String DescSintomas;
+    String Senha;
+    String Usuario;
 }
