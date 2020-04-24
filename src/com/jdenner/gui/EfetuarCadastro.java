bd7bcf2a4cae0065cf570589088c677a52088d4a
Marcos Nocetti <nocettimarcos@gmail.com>
2020-04-24 15:33:42 -0300

    Forms hotfix

--- src/com/jdenner/gui/EfetuarCadastro.java
+++ src/com/jdenner/gui/EfetuarCadastro.java
@@ -16,36 +16,13 @@ public class EfetuarCadastro extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        desktopPane = new javax.swing.JDesktopPane();
-        jLabel1 = new javax.swing.JLabel();
-        lblUser = new javax.swing.JLabel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sistema de Vendas");
-
-        desktopPane.setBackground(new java.awt.Color(153, 204, 255));
-        desktopPane.setForeground(new java.awt.Color(153, 204, 255));
-        desktopPane.setName("btDespesas"); // NOI18N
-        desktopPane.setOpaque(false);
-
-        jLabel1.setText("Usuario em uso:");
-        desktopPane.add(jLabel1);
-        jLabel1.setBounds(730, 590, 80, 40);
-
-        lblUser.setText("User");
-        desktopPane.add(lblUser);
-        lblUser.setBounds(820, 594, 60, 30);
-
-        getContentPane().add(desktopPane, java.awt.BorderLayout.CENTER);
-        desktopPane.getAccessibleContext().setAccessibleName("Despesas");
 
         setSize(new java.awt.Dimension(900, 700));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDesktopPane desktopPane;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel lblUser;
     // End of variables declaration//GEN-END:variables
 }
--- src/com/jdenner/gui/Menu.java
+++ src/com/jdenner/gui/Menu.java
@@ -16,23 +16,13 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        desktopPane = new javax.swing.JDesktopPane();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sistema de Vendas");
-
-        desktopPane.setBackground(new java.awt.Color(153, 204, 255));
-        desktopPane.setForeground(new java.awt.Color(153, 204, 255));
-        desktopPane.setName("btDespesas"); // NOI18N
-        desktopPane.setOpaque(false);
-        getContentPane().add(desktopPane, java.awt.BorderLayout.CENTER);
-        desktopPane.getAccessibleContext().setAccessibleName("Despesas");
 
         setSize(new java.awt.Dimension(900, 700));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDesktopPane desktopPane;
     // End of variables declaration//GEN-END:variables
 }
